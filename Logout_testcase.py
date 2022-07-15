f283fe281c3295bf0e132feb38d41a93ac95b17c
meg2049 <memegraj20@gmail.com>
2022-07-15 12:10:23 -0400

    Logout tescases update

--- Logout_testcase.py
+++ Logout_testcase.py
@@ -10,4 +10,9 @@ driver.get('https://www.amazon.com')
 
 driver.find_element(By.ID,'nav-orders').click()
 
+expected_result= driver.find_element(By.XPATH,"//div[@class='a-row a-spacing-base']")
+actual_result= driver.find_element(By.XPATH,"//div[@class='a-row a-spacing-base']")
+assert expected_result==actual_result
+
+print('Test cases passed')
 driver.quit()
