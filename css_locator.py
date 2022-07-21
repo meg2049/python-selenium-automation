1563f050e7c889fa78b37a4c35b74c4f3a52f381
meg2049 <memegraj20@gmail.com>
2022-07-21 18:33:53 -0400

    Css locator added

---
+++ css_locator.py
@@ -0,0 +1,29 @@
+# amazon logo By CSS selector class
+"i[class='a-icon a-icon-logo']"
+# create account logo by Css selector class
+"h1[class='a-spacing-small']"
+# Your name field by Css selector, ID
+'#ap_customer_name'
+
+# Mobile number or email field by Css selector, ID
+'#ap_email'
+
+# Password field by CSS selector, ID
+'#ap_password_check'
+
+
+# Re-Password field by CSS selector, ID
+'#ap_password'
+
+# Continue Button by CSS selector, ID
+'#continue'
+
+# Conditions of Use by Css selector, href
+
+"a[href="/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=508088']"
+
+# Privacy Notice by CSS selector, href
+
+"a[href="/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&amp;nodeId=468496']"
+
+# Sign page by CSS selector,
